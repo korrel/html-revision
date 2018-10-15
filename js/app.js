adba625b8526ddfe72a75bf0ea3d41eb1606f2ef
korrel <x.korel.x@gmail.com>
2018-10-15 16:47:09 +0200

    Dynamic text js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,48 @@
 
 $(function () {
 
-    $('.navbar-item').on('click', function() {
+    $('.navbar-item').on('click', function(/*event*/) {
         // this = event.currentTarget;
         $(".navbar-nav li").removeClass('active');
         $(this).addClass('active');
     });
+
+    /*/////////////////////////////////////////////////////*/
+
+    // var onTexte = "Sebastien Renoult";
+
+    // function ma_lettre () {
+    //     for( let i=0; i<onTexte.length; i++) {
+    //         // $(container).html(onTexte.charAt(i));
+    //         console.log(onTexte[i]);
+    //     };
+    // };
+    // setTimeout (ma_lettre,1000);
+
+
+
+    // var container = $('<h1></h1>').html();
+    // $('body').append(container);
+
+    /*/////////////////////////////////////////////////////*/
+
+
+    // effet d'apparition texte lettre par lettre
+    // trim() enlève les espaces au début et à la fin de la chaine
+    var word = $('#tap-text').text().trim();
+
+
+    // on vide la div qui contient le mot
+    $('#tap-text').empty();
+
+    //parcourir les lettres du mot
+    for (let i=0; i<word.length; i++){
+        console.log(word[i]);
+
+        setTimeout(function() {
+            console.log(i);
+            $('#tap-text').append(word[i]);
+        }, 300*i);
+    }
 
 });
